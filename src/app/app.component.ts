0132252362957d79d496daa24ba250d15c208da4
AbdallahAhmed <abdallahahmadadel@gmail.com>
2025-04-20 23:56:28 +0200

    hidden header when loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, Event } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'App-Store';
+  showHeader = true;
+
+  constructor(private router: Router) {
+    // Hide header on the loading route
+    this.router.events
+      .pipe(
+        filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd)
+      )
+      .subscribe((event: NavigationEnd) => {
+        this.showHeader = event.url !== '/';
+      });
+  }
 }
