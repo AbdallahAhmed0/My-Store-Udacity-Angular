d708354ef3a59d6430748ef4919ceed1bda15604
AbdallahAhmed <abdallahahmadadel@gmail.com>
2025-04-20 23:28:47 +0200

    edit routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { CartComponent } from './Componants/cart/cart.component';
 import { ConfirmationComponent } from './Componants/confirmation/confirmation.component';
 import { ProductItemDetailComponent } from './Componants/product-item-detail/product-item-detail.component';
 import { ProductListComponent } from './Componants/product-list/product-list.component';
+import { LoadingComponent } from './Componants/loading/loading.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'/products',pathMatch:'full'},
+  { path: '', component: LoadingComponent },
+  {path:'products',redirectTo:'/products',pathMatch:'full'},
   {path:'products',component:ProductListComponent},
   {path:'product/:id',component:ProductItemDetailComponent},
   {path:'cart',component:CartComponent},
